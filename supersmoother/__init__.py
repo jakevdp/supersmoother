267ddad7df9d4df78ac2b8e1b826095eaa71b790
Jake VanderPlas <jakevdp@gmail.com>
2015-08-19 06:48:31 -0700

    MAINT: bump version to 0.4.dev

--- supersmoother/__init__.py
+++ supersmoother/__init__.py
@@ -1,7 +1,7 @@
 """Python implementation of Friedman's Supersmoother"""
 from __future__ import absolute_import
 
-__version__ = '0.3.2'
+__version__ = '0.4.dev0'
 
 from .smoother import MovingAverageSmoother, LinearSmoother
 from .supersmoother import SuperSmoother
